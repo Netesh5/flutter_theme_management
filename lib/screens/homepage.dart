50b413d94e16aaa22fe0aeedce952a5b4b435c9b
Netesh5 <netesh.paudel@gmail.com>
2022-04-27 12:30:16 +0545

    homepage creation

---
+++ lib/screens/homepage.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Flutter Theme Demo"),
+        ),
+        body: const Center(
+          child: Text("Theme Demo"),
+        ));
+  }
+}
